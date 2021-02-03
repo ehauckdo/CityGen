3ac8a93040d3b358fda237c4c5b3aba1b6ab7177
ehauckdo <eduardohauck@gmail.com>
2021-02-03 14:08:48 +0900

    removing dead code

--- experiment/experiment/classifier/model.py
+++ experiment/experiment/classifier/model.py
@@ -31,23 +31,3 @@ def accuracy(image_filename, model):
 
     averageValue = sum(value)/len(value)
     return averageValue
-
-def evaluate(image_filename, model_filename="classifier/weight_tsukuba.hdf5"):
-    model = val.createModel(model_filename)
-    mymap =  map.readFile(image_filename)
-
-    imageName = "temp" #do not use file type Already provided by the lib just
-                       # use the name, path is allowed
-    renderer.render(mymap)
-    renderer.osmToImage(imageName)
-
-    test = io.imread(imageName + ".png")
-
-    splittedImage = val.imageSplitter(test)
-
-    value = val.validate(model, splittedImage)
-
-    clean_files(imageName)
-
-    averageValue = sum(value)/len(value)
-    return averageValue
